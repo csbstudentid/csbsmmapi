60264bf327fe419b47ee7ea0d0706f45cb015a85
csbstudentid <cyberbijoy121@gmail.com>
2026-08-17 14:23:36 +0600

    Update v2.js

--- api/v2.js
+++ api/v2.js
@@ -1,5 +1,4 @@
-export default async function handler(req, res) {
-  // CORS হেডার
+module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
@@ -8,7 +7,6 @@ export default async function handler(req, res) {
     return res.status(200).end();
   }
 
-  // 🔒 বটের গোপন ক্রেডেনশিয়াল
   const BOT_ID = "2890129";
   const PUBLIC_TOKEN = "cfab0a2692a9ba1b3712a6af4e031ad6";
   const COMMAND = "child_handler";
@@ -21,16 +19,16 @@ export default async function handler(req, res) {
     const queryString = new URLSearchParams(queryParams).toString();
     const targetUrl = `https://api.bots.business/v1/bots/${BOT_ID}/new-webhook?${queryString}`;
 
-    const bbResponse = await fetch(targetUrl);
-    const rawData = await bbResponse.text();
+    const response = await fetch(targetUrl);
+    const textData = await response.text();
 
     try {
-      const jsonData = JSON.parse(rawData);
-      return res.status(200).json(jsonData);
+      const json = JSON.parse(textData);
+      return res.status(200).json(json);
     } catch (e) {
-      return res.status(200).send(rawData);
+      return res.status(200).send(textData);
     }
   } catch (error) {
-    return res.status(500).json({ error: "Gateway Error: " + error.message });
+    return res.status(500).json({ error: error.message });
   }
-}
+};
