36377bcfad742a9d54744daef23b5135661b33f7
csbstudentid <cyberbijoy121@gmail.com>
2026-08-17 14:15:26 +0600

    Update v2.js

--- api/v2.js
+++ api/v2.js
@@ -1,6 +1,4 @@
-const https = require('https');
-
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
@@ -13,30 +11,24 @@ module.exports = (req, res) => {
   const PUBLIC_TOKEN = "cfab0a2692a9ba1b3712a6af4e031ad6";
   const COMMAND = "child_handler";
 
-  const queryParams = Object.assign({}, req.query || {}, req.body || {});
-  queryParams.command = COMMAND;
-  queryParams.public_user_token = PUBLIC_TOKEN;
+  try {
+    const queryParams = Object.assign({}, req.query || {}, req.body || {});
+    queryParams.command = COMMAND;
+    queryParams.public_user_token = PUBLIC_TOKEN;
 
-  const queryString = new URLSearchParams(queryParams).toString();
-  const targetUrl = `https://api.bots.business/v1/bots/${BOT_ID}/new-webhook?${queryString}`;
+    const queryString = new URLSearchParams(queryParams).toString();
+    const targetUrl = `https://api.bots.business/v1/bots/${BOT_ID}/new-webhook?${queryString}`;
 
-  https.get(targetUrl, (apiRes) => {
-    let rawData = '';
+    const response = await fetch(targetUrl);
+    const textData = await response.text();
 
-    apiRes.on('data', (chunk) => {
-      rawData += chunk;
-    });
-
-    apiRes.on('end', () => {
-      res.setHeader('Content-Type', 'application/json');
-      try {
-        const json = JSON.parse(rawData);
-        res.status(200).json(json);
-      } catch (e) {
-        res.status(200).send(rawData);
-      }
-    });
-  }).on('error', (err) => {
-    res.status(500).json({ error: "Gateway Error: " + err.message });
-  });
+    try {
+      const json = JSON.parse(textData);
+      return res.status(200).json(json);
+    } catch (e) {
+      return res.status(200).send(textData);
+    }
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
 };
