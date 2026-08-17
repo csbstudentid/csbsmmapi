e132410e86976dd562409ab43bc7bf87356344fb
csbstudentid <cyberbijoy121@gmail.com>
2026-08-17 14:34:27 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-export default async function handler(req, res) {
+const https = require('https');
+const url = require('url');
+
+module.exports = (req, res) => {
   // CORS হেডার
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
   if (req.method === 'OPTIONS') {
-    return res.status(200).end();
+    if (typeof res.status === 'function') return res.status(200).end();
+    res.writeHead(200);
+    return res.end();
   }
 
   // 🔒 বটের গোপন ক্রেডেনশিয়াল
@@ -14,23 +19,44 @@ export default async function handler(req, res) {
   const COMMAND = "child_handler";
 
   try {
-    const queryParams = Object.assign({}, req.query || {}, req.body || {});
+    const parsedUrl = url.parse(req.url, true);
+    const queryParams = Object.assign({}, parsedUrl.query, req.query || {}, req.body || {});
     queryParams.command = COMMAND;
     queryParams.public_user_token = PUBLIC_TOKEN;
 
     const queryString = new URLSearchParams(queryParams).toString();
     const targetUrl = `https://api.bots.business/v1/bots/${BOT_ID}/new-webhook?${queryString}`;
 
-    const bbResponse = await fetch(targetUrl);
-    const textData = await bbResponse.text();
-
-    try {
-      const jsonData = JSON.parse(textData);
-      return res.status(200).json(jsonData);
-    } catch (e) {
-      return res.status(200).send(textData);
+    https.get(targetUrl, (apiRes) => {
+      let rawData = '';
+      apiRes.on('data', (chunk) => { rawData += chunk; });
+      apiRes.on('end', () => {
+        res.setHeader('Content-Type', 'application/json; charset=utf-8');
+        if (typeof res.status === 'function') {
+          res.status(200).send(rawData);
+        } else {
+          res.writeHead(200);
+          res.end(rawData);
+        }
+      });
+    }).on('error', (err) => {
+      res.setHeader('Content-Type', 'application/json');
+      const errJson = JSON.stringify({ error: "Gateway Error: " + err.message });
+      if (typeof res.status === 'function') {
+        res.status(500).send(errJson);
+      } else {
+        res.writeHead(500);
+        res.end(errJson);
+      }
+    });
+  } catch (err) {
+    res.setHeader('Content-Type', 'application/json');
+    const errJson = JSON.stringify({ error: "Server Error: " + err.message });
+    if (typeof res.status === 'function') {
+      res.status(500).send(errJson);
+    } else {
+      res.writeHead(500);
+      res.end(errJson);
     }
-  } catch (error) {
-    return res.status(500).json({ error: "Gateway Error: " + error.message });
   }
-}
+};
